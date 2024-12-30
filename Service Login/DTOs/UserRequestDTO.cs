ae577e1644bdc0b63636217b3c78e6d9046b7cf2
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2024-12-30 17:33:55 +0100

    making a Feature/UserServiceImplementation was dumb, so I removed it

--- Service Login/DTOs/UserRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Service_Login.DTOs;
-
-public class UserRequestDTO
-{
-    public long GitId { get; set; }
-    public string Username { get; set; }
-    public string Avatar { get; set; }
-}
--- Service Login/Models/User.cs
+++ Service Login/Models/User.cs
@@ -2,8 +2,8 @@ namespace Service_Login.Models;
 
 public class User
 {
-    public Guid Id { get; set; }
-    public long GitId { get; set; }
-    public string Username { get; set; }
-    public string Avatar { get; set; }
+    public Guid id { get; set; }
+    public long gitId { get; set; }
+    public string username { get; set; }
+    public string avatar { get; set; }
 }
